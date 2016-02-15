dfb290e8d4c0fd8b4ba41b40443e7939e3b4ecef
Gordon Emmerson <gordon1470@gmail.com>
2016-02-14 22:05:46 -0600

    changed char -> binary array generation method, needs testing

--- USB_UART01.cydsn/main.c
+++ USB_UART01.cydsn/main.c
@@ -38,10 +38,10 @@ enum crc {none, header, message, both} crcState;
 
 char receivedChar;
 
-CY_ISR(Idle_Collision_ISR){ 
-    networkState = idle; 
+CY_ISR(Idle_Collision_ISR){
+    networkState = idle;
     if(Receive_Read() == HIGH){
-        networkState = idle;  
+        networkState = idle;
     }
     else{
         networkState = collision;
@@ -51,12 +51,12 @@ CY_ISR(Idle_Collision_ISR){
 CY_ISR(Edge_detect_ISR){
     //Note: on edge detect, will trigger receive timer to start (see TopDesign)
     Idle_Collision_Timer_Start();
-	
-    //Ignore power on edge 
+
+    //Ignore power on edge
     if(!powerOnEdge){
-        Receive_Timer_Start(); 
+        Receive_Timer_Start();
         diffManReceivedData[receivedDataCount] = Receive_Read();
-        receivedDataCount++;    
+        receivedDataCount++;
     }
     else{
         powerOnEdge = false;
@@ -86,33 +86,33 @@ int main()
     uint8 stringPosition = HEADER_POS;//after header
     timerExpired = false;
     dataTransmissionComplete = false;
-	
+
 	messageLength = 0;
-	
-    powerOnEdge = true; //When the system powers up, it creates a rising edge. 
+
+    powerOnEdge = true; //When the system powers up, it creates a rising edge.
     //We want to ignore this edge on the rising edge.
-	
+
     /*
     Get value from system clock and
     place in seconds variable.
     */
-    time_t seconds; 
+    time_t seconds;
     time(&seconds);
     /*
     Convert seconds to a unsigned
     integer for seed for random number generator
     */
     srand((unsigned int) seconds);
-            
+
     TX_pin_Write(1);  //set TX line to high to start
-    
+
     /* Enable Global Interrupts */
-    CyGlobalIntEnable;  
-    
+    CyGlobalIntEnable;
+
     Receive_ClearInterrupt();//TODO clearing first edge
-    
+
     LCD_Start();
-    
+
     //enable collision detection
     Idle_Collision_IRQ_StartEx(Idle_Collision_ISR);
     Edge_detect_IRQ_StartEx(Edge_detect_ISR);
@@ -120,40 +120,40 @@ int main()
 
     /* Start USBFS Operation with 3V operation */
     USBUART_1_Start(0u, USBUART_1_3V_OPERATION);
-    
+
     //start transmission timer
     TimerISR_StartEx(TimerHandler);
 
-    ReceiveTimerIRQ_StartEx(ReceiveTimerISR); 
-    
+    ReceiveTimerIRQ_StartEx(ReceiveTimerISR);
+
     receivedDataCount = 0;
-    receivedDataIndex = 0; 
-   
+    receivedDataIndex = 0;
+
     lineString[0] = 0x71;
 	lineString[1] = VERSION_NUMBER;
 	lineString[2] = TX_SOURCE_ADDRESS;
 	lineString[3] = TX_DESTINATION_ADDRESS;
-    lineString[4] = 0; 
+    lineString[4] = 0;
 	lineString[5] = 0;
 	lineString[6] = 0x75;
-	
+
     /* Main Loop: */
     for(;;)
     {
         setNetworkStateOnLEDs();
-        
+
         /*Receive*/
         //Precondidtion: must finished receiving data so channel state machine at idle and wait for a char
         if(networkState == idle && receivedDataCount >= 34){//TODO remove hardcode #
-            
+
             //Verify that have received start bit (01). Note: ignore first edge from turning system on
             if(diffManReceivedData[0] == 0 && diffManReceivedData[1] == 1){
                 receivedDataIndex = 2; //skip start bit (two half bits)
-                while(receivedDataIndex < receivedDataCount-1){     //Note: receivedDataCount-1 b/c gets one extra bit from Receive_Timer expiring 
+                while(receivedDataIndex < receivedDataCount-1){     //Note: receivedDataCount-1 b/c gets one extra bit from Receive_Timer expiring
                     //wait for 8 bits TODO bad comment
                     //char receivedChar;     TODO move to global variable
                     //char *charPtr = &receivedChar;
-                    
+
                     diffManToASCII();
 					storeChar();
                     receivedChar = 0;       //Reset the char
@@ -168,13 +168,13 @@ int main()
                     LCD_PrintNumber(diffManReceivedData[i]);
                 }
                 LCD_Position(1,0);
-                
+
                 for(i=16; i<32;i++){
                     LCD_PrintNumber(diffManReceivedData[i]);
                 }*/
                 //end remove
-                
-                CyDelay(1000);//TODO remove for testing only    
+
+                CyDelay(1000);//TODO remove for testing only
                 LCD_ClearDisplay();//TODO remove
                 receivedDataCount = 0;  //Reset count
                 receivedDataIndex = 0;  //Reset index
@@ -183,7 +183,7 @@ int main()
             else{
                 //Receive data did not start with the start bit
                 receivedDataCount = 0;
-                receivedDataIndex = 0; 
+                receivedDataIndex = 0;
                 currentRXCharPosition = 0;
             }
         }
@@ -192,7 +192,7 @@ int main()
             //TODO prob remove this else
         }
         /*End Receive*/
-        
+
         if(USBUART_1_IsConfigurationChanged() != 0u) /* Host could send double SET_INTERFACE request */
         {
             if(USBUART_1_GetConfiguration() != 0u)   /* Init IN endpoints when device configured */
@@ -200,11 +200,11 @@ int main()
                 /* Enumeration is done, enable OUT endpoint for receive data from Host */
                 USBUART_1_CDC_Init();
             }
-        }         
+        }
         if(USBUART_1_GetConfiguration() != 0u)    /* Service USB CDC when device configured */
         {
             if(USBUART_1_DataIsReady() != 0u)               /* Check for input data from PC */
-            {   
+            {
                 rx = USBUART_1_GetChar();
                 switch(rx){
                     case 8://backspace
@@ -212,7 +212,7 @@ int main()
                             stringPosition--;
                             lineString[stringPosition] = '\0';
                             //backspace space backspace
-                            while(USBUART_1_CDCIsReady() == 0u); 
+                            while(USBUART_1_CDCIsReady() == 0u);
                             USBUART_1_PutChar(8);
                             USBUART_1_PutChar(32);
                             USBUART_1_PutChar(8);
@@ -224,11 +224,11 @@ int main()
                         stringToDiffMan(lineString, stringPosition);
                         while(USBUART_1_CDCIsReady() == 0u);
                         USBUART_1_PutCRLF();
-                        
+
                         //keep looping until data is transmitted
-						
+
                         while(!dataTransmissionComplete){//header is mostly encoded already, just need to put in message length
-							
+
                             transmitData();
                             setNetworkStateOnLEDs();
                         }
@@ -236,25 +236,25 @@ int main()
                         TX_pin_Write(1);    //set line to logic-1 after transmission
                         //reset index
                         halfBitIndex = 0;
-                        stringPosition = HEADER_POS; 
+                        stringPosition = HEADER_POS;
                         break;
                     case 27://escape
                         break;
                     default://everything else
                         lineString[stringPosition] = rx;
                         stringPosition++;
-                        while(USBUART_1_CDCIsReady() == 0u); 
+                        while(USBUART_1_CDCIsReady() == 0u);
                         USBUART_1_PutChar(rx);       /* Send data back to PC */
                         break;
                 }
             }
         }
-    }   
+    }
 }
 
 /*
-init dif man encoded data array with a leading "starting bit". The starting bit is madeup of the 
-half bits 01. 
+init dif man encoded data array with a leading "starting bit". The starting bit is madeup of the
+half bits 01.
 Only call when starting to transfer a new string
 */
 void initDiffManEncodedArray(){
@@ -295,23 +295,25 @@ void asciiToDiffMan(char asciiChar)
         halfBitIndex++;
         diffManEncodedData[halfBitIndex] = 1;
         halfBitIndex++;
-    } 
+    }
     previousHalfBit = diffManEncodedData[halfBitIndex-1];
 
-    
-    //convert asciil char to binary value (which will be 7 bits)
-    unsigned int binaryValueOfChar[20];//index zero is LSB 
-    int i;
-    for(i=0; asciiChar>0; i++)//TODO: need new solution to convert from char to array of binary
-    { 
-        binaryValueOfChar[i]=asciiChar%2; 
-        asciiChar=asciiChar/2;
-    }
 
+    //convert asciil char to binary value (which will be 7 bits)
+    unsigned int binaryValueOfChar[20];//index zero is LSB
+    int i;
+    /*for(i=0; asciiChar>0; i++)
+    {
+        binaryValueOfChar[i]=asciiChar%2;
+        asciiChar=asciiChar/2;
+    }*/
+    for (i = 0; i < 8; ++i){//todo test new value
+        binaryValueOfChar[i] = (mychar >> i) & 1;
+    }
     //differential encode the 7 bits (from the binary version of the char)
-    //must start at the end of the array so to encode the MSB first 
+    //must start at the end of the array so to encode the MSB first
     int j;
-    for(j=INDEX_OF_MSB;j>=0;j--) 
+    for(j=INDEX_OF_MSB;j>=0;j--)
     {
         if(binaryValueOfChar[j] == 1)
         {
@@ -321,7 +323,7 @@ void asciiToDiffMan(char asciiChar)
                 diffManEncodedData[halfBitIndex] = 1;
                 halfBitIndex++;
                 diffManEncodedData[halfBitIndex] = 0;
-                halfBitIndex++;    
+                halfBitIndex++;
             }
             else
             {
@@ -330,7 +332,7 @@ void asciiToDiffMan(char asciiChar)
                 diffManEncodedData[halfBitIndex] = 1;
                 halfBitIndex++;
             }
-            
+
         }
         else
         {
@@ -350,10 +352,10 @@ void asciiToDiffMan(char asciiChar)
                 halfBitIndex++;
             }
         }
-        
+
         previousHalfBit = diffManEncodedData[halfBitIndex-1];
     }//end for
-   
+
 }//end function
 
 /*
@@ -364,13 +366,13 @@ Note: halfBitIndex will be equal to the size of the diffManEncodedData array at 
 void transmitData(){
     int i;
     for(i = 0; i < halfBitIndex; i++){
-        //check for idle, if network is idle, cotinue to transmit data. 
+        //check for idle, if network is idle, cotinue to transmit data.
         //Else break out of transmition and wait random time
         if(networkState != idle){
             if(networkState == collision){
                 TX_pin_Write(1);
                 int value = (getRandomNumber()/128.0)*800.0;
-               
+
                 //Back-off a random time between 0 and 0.8 seconds
                 CyDelay(value);
                 break;
@@ -378,11 +380,11 @@ void transmitData(){
         }
         TX_pin_Write(diffManEncodedData[i]);
         Timer_Start();
-        while(!timerExpired); 
+        while(!timerExpired);
         timerExpired = false;
     }
-    
-    //when finished transmitting diff manchester encoded data, set flag 
+
+    //when finished transmitting diff manchester encoded data, set flag
     if(i == halfBitIndex){
         dataTransmissionComplete = true;
     }
@@ -415,7 +417,7 @@ void setNetworkStateOnLEDs(){
         {
            //error
             break;
-        } 
+        }
 	}
 }
 
@@ -426,7 +428,7 @@ int getRandomNumber()
 {
     const int MIN_N = 1;
     const int MAX_N = 128;
-        
+
     int n;
 /*
     Get first and second random numbers.
@@ -439,7 +441,7 @@ int getRandomNumber()
 Helper method.
 Convert differental manchester line encoded byte to ascii char
 
-charPtr = pointer to char 
+charPtr = pointer to char
 */
 void diffManToASCII()
 {
@@ -451,30 +453,30 @@ void diffManToASCII()
             //current half bit is 1
             if(previousHalfBit == 1)
             {
-                receivedChar |= (1 << (7 - i));      
+                receivedChar |= (1 << (7 - i));
             }
             else
             {
-                receivedChar |= (0 << (7 - i));    
+                receivedChar |= (0 << (7 - i));
             }
         }
         else{
             //current half bit is 0
             if(previousHalfBit == 1)
             {
-                receivedChar |= (0 << (7 - i));    
+                receivedChar |= (0 << (7 - i));
             }
             else
             {
-                receivedChar |= (1 << (7 - i));    
+                receivedChar |= (1 << (7 - i));
             }
-        } 
+        }
         receivedDataIndex += 2;
     }//end for loop
 }
 
 //stores char in receive array
-void storeChar(){   
+void storeChar(){
 	receivedChar &= ASCII_CHAR_MASK;
 	rxChar[currentRXCharPosition] = receivedChar;
 	currentRXCharPosition++;
@@ -484,7 +486,7 @@ void storeChar(){
 void printChar(){
 	int i;//char position after array
     for(i = 7; i < messageLength; i++){//we are assuming messageLength from the header is valid and corresponds to currentRXCharPosition
-		LCD_PutChar(rxChar[i]); 
+		LCD_PutChar(rxChar[i]);
 		//TODO: implement USB transmit
     }
 }
@@ -497,7 +499,7 @@ bool headerCheck(){
 		if(rxChar[3] == 0x00 || rxChar[3] == RX_DESTINATION_ADDRESS){
 			//this is as valid as we go (CRC is optional)
 			messageLength = rxChar[4];
-            LCD_PrintNumber(rxChar[4]); 
+            LCD_PrintNumber(rxChar[4]);
 			crcState = rxChar[5];
 			headerCRC = rxChar[6];
             CyDelay(1000);
